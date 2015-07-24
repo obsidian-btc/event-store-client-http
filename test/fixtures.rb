2f1d628b4c6f8f07eb2d7a20df6d8613357d3718
Scott Bellware <sbellware@gmail.com>
2015-07-24 01:28:56 -0500

    Obsolete and redundant fixtures are removed

--- test/fixtures.rb
+++ test/fixtures.rb
@@ -18,147 +18,12 @@ module Fixtures
     end
   end
 
-  # module ATOM
-  #   module Document
-  #     def self.data
-  #       JSON.parse(text)
-  #     end
-
-  #     def self.text
-  #       File.read(filepath)
-  #     end
-
-  #     def self.filepath
-  #       pathname = Pathname.new __FILE__
-  #       pathname = Pathname.new pathname.dirname
-  #       pathname += 'data/someStream.json'
-  #       pathname.to_s
-  #     end
-  #   end
-  # end
-
   module Stream
     def self.name(category=nil, id=nil)
       category ||= 'test'
       id ||= UUID.random
       category_name = "#{category}#{UUID.random.gsub('-', '')}"
       "#{category_name}-#{id}"
-    end
-
-    module Slice
-      def self.data
-        JSON.parse(text)
-      end
-
-      def self.text
-        File.read(filepath)
-      end
-
-      def self.filepath
-        pathname = Pathname.new __FILE__
-        pathname = Pathname.new pathname.dirname
-        pathname += 'data/slice.json'
-        pathname.to_s
-      end
-    end
-
-    module Entry
-      def self.data(i=nil)
-        i ||= 1
-
-        id = ID.get(i)
-
-        {
-          id: id,
-          type: 'SomeEvent',
-          number: 1,
-          position: 11,
-          stream_name: 'someStream',
-          uri: 'http://127.0.0.1:2113/streams/someStream/1',
-          created_time: '2015-06-08T04:37:01.066935Z',
-          data: {
-            'some_attribute' => 'some value',
-            'some_time' => '2015-06-07T23:37:01Z'
-          },
-          metadata: {
-            "some_meta_attribute" => "some meta value"
-          }
-        }
-      end
-
-      module JSON
-        def self.data(i=nil)
-          i ||= 1
-
-          id = ID.get(i)
-
-          {
-            eventId: id,
-            eventType: 'SomeEvent',
-            eventNumber: 1,
-            positionEventNumber: 11,
-            streamId: 'someStream',
-            id: 'http://127.0.0.1:2113/streams/someStream/1',
-            updated: '2015-06-08T04:37:01.066935Z',
-            data: {
-              'someAttribute' => 'some value',
-              'someTime' => '2015-06-07T23:37:01Z'
-            },
-            metaData: {
-              "someMetaAttribute" => "some meta value"
-            }
-          }
-        end
-
-        def self.text
-          data.to_json
-        end
-
-        def self.list(count=nil)
-          count ||= 1
-
-          list = []
-
-          count.times do |i|
-            i += 1
-            entry = data(i)
-            list << entry
-          end
-
-          list
-        end
-
-        module Raw
-          def self.data
-            ::JSON.parse(text)
-          end
-
-          def self.text
-            File.read(filepath)
-          end
-
-          def self.list(count=nil)
-            count ||= 1
-
-            list = []
-
-            count.times do |i|
-              i += 1
-              entry = data
-              list << entry
-            end
-
-            list
-          end
-
-          def self.filepath
-            pathname = Pathname.new __FILE__
-            pathname = Pathname.new pathname.dirname
-            pathname += 'data/entry.json'
-            pathname.to_s
-          end
-        end
-      end
     end
   end
 
@@ -194,7 +59,6 @@ module Fixtures
               }
             ]
           }
-
         end
 
         def self.text
@@ -219,12 +83,6 @@ module Fixtures
       event_data.metadata = Metadata.data
 
       event_data
-    end
-
-    def self.json_text(time=nil)
-      time ||= Time.now.iso8601(5)
-      data_text = '"eventId":"10000000-0000-0000-0000-000000000000","eventType":"SomeEvent","data":{"someAttribute":"some value"}'
-      "{#{data_text},#{Metadata.json_text}}"
     end
 
     def self.write(count=nil, stream_name=nil)
