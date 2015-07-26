62cbcf663803bd9ba963a15b5521972c2185465f
Scott Bellware <sbellware@gmail.com>
2015-07-26 17:14:40 -0500

    Batch can be constructed from a list of event data

--- test/spec/batch_construction.rb
+++ test/spec/batch_construction.rb
@@ -10,3 +10,16 @@ describe "Construct a Batch from a Single EventData" do
     assert(batch.any? event_data)
   end
 end
+
+describe "Construct a Batch from a list of EventData" do
+  specify "The single EventData is added to the batch" do
+    event_data_1 = Fixtures::EventData::Write.example
+    event_data_2 = Fixtures::EventData::Write.example
+
+    batch = EventStore::Client::HTTP::EventData::Batch.build [event_data_1, event_data_2]
+
+    assert(batch.length == 2)
+    assert(batch.any? event_data_1)
+    assert(batch.any? event_data_2)
+  end
+end
