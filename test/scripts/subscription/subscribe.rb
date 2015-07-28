29663093b79245a31b0a6ea11d71908f88e23b97
Scott Bellware <sbellware@gmail.com>
2015-07-28 15:57:26 -0500

    Instructions are printed if the subscribe script is run without running the event writer script

--- test/scripts/subscription/subscribe.rb
+++ test/scripts/subscription/subscribe.rb
@@ -1,9 +1,10 @@
 require_relative './subscription_init'
 
-stream_name = File.read "tmp/stream_name"
-
-unless stream_name
-  raise "Stream name file is missing (tmp/stream_name). It is created by the write_events_periodically.rb script."
+stream_name = nil
+begin
+  stream_name = File.read "tmp/stream_name"
+rescue
+  raise "Stream name file is missing (tmp/stream_name). It is created by the write_events_periodically.rb script, which must be run concurrently with #{__FILE__}."
 end
 
 event_reader = EventStore::Client::HTTP::EventReader.build stream_name, slice_size: 1
