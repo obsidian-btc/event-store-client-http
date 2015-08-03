6ee62c51ecf9623d94971c6b09db571df5d6ad3c
Scott Bellware <sbellware@gmail.com>
2015-08-02 23:49:21 -0500

    Integrated tests are included in the suite

--- test/spec.rb
+++ test/spec.rb
@@ -1,5 +1,5 @@
 require_relative 'test_init'
 
 Runner.! 'spec/**/*.rb' do |exclude|
-  exclude =~ /integration|(_init.rb|\.scratch.rb|\.skip.rb)\z/
+  exclude =~ /integration\.rb|(_init.rb|\.scratch.rb|\.skip\.rb)\z/
 end
