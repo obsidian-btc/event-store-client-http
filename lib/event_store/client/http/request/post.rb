135f3e1e97a026c7dd535e739cd51a10b19d27f4
Scott Bellware <sbellware@gmail.com>
2015-08-02 23:24:36 -0500

    Expected version tests and corrections

--- lib/event_store/client/http/request/post.rb
+++ lib/event_store/client/http/request/post.rb
@@ -3,13 +3,15 @@ module EventStore
     module HTTP
       module Request
         class Post
+          class ExpectedVersionError < RuntimeError; end
+
           include Request
 
           def !(data, path, expected_version: nil)
-            logger.info "Posting to #{path}"
+            logger.trace "Posting to #{path}"
             logger.data data
 
-            response = post(data, path)
+            response = post(data, path, expected_version)
 
             logger.info "POST Response\nPath: #{path}\nStatus: #{(response.code + " " + response.message).rstrip}"
             logger.debug "Posted to #{path}"
@@ -17,18 +19,28 @@ module EventStore
             response
           end
 
-          def post(data, path)
-            request = build_request(data, path)
-            client.request(request)
+          def post(data, path, expected_version=nil)
+            request = build_request(data, path, expected_version)
+
+            response = client.request(request)
+
+            if "#{response.code} #{response.message}" == "400 Wrong expected EventNumber"
+              raise ExpectedVersionError, "Wrong expected version number: #{expected_version} (Path: #{path})"
+            end
+
+            response
           end
 
           def build_request(data, path, expected_version=nil)
+            logger.trace "Building request (Path: #{path}, Expected Version: #{!!expected_version ? expected_version : '(none)'}"
             request = Net::HTTP::Post.new(path)
 
             set_event_store_content_type_header(request)
-            set_expected_version_header(request, expected_version) if expected_version
+            set_expected_version_header(request, expected_version) if !!expected_version
 
             request.body = data
+
+            logger.debug "Built request (Path: #{path}, Expected Version: #{!!expected_version ? expected_version : '(none)'}"
 
             request
           end
---
+++ test/spec/database_integration/expected_version.rb
@@ -0,0 +1,35 @@
+require_relative './database_integration_init'
+
+describe "Writing the Expected Version Number" do
+  describe "Right Version" do
+    stream_name = EventStore::Client::HTTP::Controls::StreamName.get 'testWrongVersion'
+    path = "/streams/#{stream_name}"
+
+    writer = EventStore::Client::HTTP::EventWriter.build
+
+    event_data_1 = EventStore::Client::HTTP::Controls::EventData::Write.example
+    result = writer.write event_data_1, stream_name
+
+    event_data_2 = EventStore::Client::HTTP::Controls::EventData::Write.example
+
+    writer.write event_data_2, stream_name, expected_version: 0
+  end
+
+  describe "Wrong Version" do
+    stream_name = EventStore::Client::HTTP::Controls::StreamName.get 'testWrongVersion'
+    path = "/streams/#{stream_name}"
+
+    writer = EventStore::Client::HTTP::EventWriter.build
+
+    event_data_1 = EventStore::Client::HTTP::Controls::EventData::Write.example
+    result = writer.write event_data_1, stream_name
+
+    event_data_2 = EventStore::Client::HTTP::Controls::EventData::Write.example
+
+    specify "Fails" do
+      assert_raises(EventStore::Client::HTTP::Request::Post::ExpectedVersionError) do
+        writer.write event_data_2, stream_name, expected_version: 11
+      end
+    end
+  end
+end
