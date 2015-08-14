609d2ca6d8f4404bd3af062a9f32603956d1d298
Scott Bellware <sbellware@gmail.com>
2015-08-13 19:42:34 -0500

    Test for get of stream that doesn't exist

---
+++ test/spec/database_integration/get_not_found.rb
@@ -0,0 +1,19 @@
+require_relative './database_integration_init'
+
+describe "Get a Stream that Doesn't Exist" do
+  stream_name = EventStore::Client::HTTP::Controls::StreamName.get "testNotFound"
+  path = "/streams/#{stream_name}"
+
+  data = EventStore::Client::HTTP::Controls::EventData::Batch::JSON.text
+
+  get = EventStore::Client::HTTP::Request::Get.build
+  body_text, response = get.! path
+
+  specify "Response status is 404 Not Found" do
+    assert(response.is_a? Net::HTTPNotFound)
+  end
+
+  specify "Body is empty" do
+    assert(body_text.empty?)
+  end
+end
