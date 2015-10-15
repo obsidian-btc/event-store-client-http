3424827b0940b196dd9a335706c11c76ddba20fd
ntl <nathanladd+github@gmail.com>
2015-10-15 12:01:37 -0500

    Clarify get/post API

--- lib/event_store/client/http/request/post.rb
+++ lib/event_store/client/http/request/post.rb
@@ -23,7 +23,7 @@ module EventStore
           def post(data, path, expected_version=nil)
             request = build_request(path, expected_version)
 
-            response = session.request(request, request_body: data)
+            response = session.post(request, data)
 
             if "#{response.status_code} #{response.reason_phrase}" == "400 Wrong expected EventNumber"
               raise ExpectedVersionError, "Wrong expected version number: #{expected_version} (Path: #{path})"
